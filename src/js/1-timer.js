7d540ed98f6233f6ac3f53dfde6b8416e387deb1
fokasiyy <foka123zate@gmail.con>
2025-06-01 16:45:49 +0300

    add markup header

---
+++ src/js/1-timer.js
@@ -0,0 +1,106 @@
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+import { split } from "postcss/lib/list";
+
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+const input = document.querySelector('input');
+
+const btn = document.querySelector('button');
+
+let selectedDatesTime = null;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+
+
+      const date = new Date();
+      if(selectedDates[0] > date){
+        selectedDatesTime = selectedDates[0];
+
+        btn.removeAttribute('disabled', '');
+
+      }else{
+        btn.setAttribute('disabled', '');
+        iziToast.show({              
+          message: 'Please choose a date in the future'
+      })
+      }
+    },
+    
+  };
+
+  btn.addEventListener('click', clockTime);
+
+
+  function clockTime(){
+    input.setAttribute('disabled', '');
+    btn.setAttribute('disabled', '');
+    const intervalId = setInterval(() =>{
+      const now = new Date();
+      const mili = selectedDatesTime - now;
+
+      if(mili <= 0){
+        clearInterval(intervalId);
+        input.removeAttribute('disabled', '');
+        updateClock(0, 0, 0, 0);
+        btn.removeEventListener('click', clockTime);
+        
+        return;
+      }
+
+
+
+      function convertMs(ms){
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+      
+        // Remaining days
+        const days = Math.floor(ms / day);
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+      
+        return {days, hours, minutes, seconds};}
+      
+
+      
+      const resultMathTime = convertMs(mili);
+      
+
+      updateClock(resultMathTime.days, resultMathTime.hours, resultMathTime.minutes, resultMathTime.seconds);
+
+       
+
+    }, 1000);
+  }
+  
+  function updateClock(days, hours, minutes, seconds){
+    dataDays.textContent = String(days).padStart(2, '0');
+    dataHours.textContent = String(hours).padStart(2, '0');
+    dataMinutes.textContent = String(minutes).padStart(2, '0');
+    dataSeconds.textContent = String(seconds).padStart(2, '0');
+  }
+
+
+
+  
+flatpickr('#datetime-picker', options);
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,66 @@
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+const input = document.querySelector('[name="delay"]');
+
+const form = document.querySelector('form');
+
+
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const form = event.currentTarget;
+    const delay = input.value;
+    const radio = document.querySelector('input[name="state"]:checked');
+
+
+    const result = new Promise((resolve, reject) =>{
+
+        
+
+        setTimeout(() =>{
+
+            if(radio){
+                if(radio.value === 'fulfilled'){
+                    resolve(`FullFilled promise in ${delay}`);
+                        
+
+                    
+
+                }
+
+                else{
+                    reject(`Rejected promise in ${delay}`)
+                        
+                    
+                }
+
+            }
+
+        }, delay);
+    })
+
+    result
+    .then(value =>{
+        iziToast.show({              
+            message: `FullFilled promise in ${delay}`
+        })
+
+    })
+    .catch(
+        error =>{
+            iziToast.show({
+                            
+                message: `Rejected promise in ${delay}`
+            })
+
+        }
+    )
+
+
+    form.reset();
+
+})
